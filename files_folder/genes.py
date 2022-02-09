b499610b407afc858117adb98416734bd14adde3
kh-19 <64449750+kh-19@users.noreply.github.com>
2022-02-09 10:06:42 -0800

    Update genes.py

--- files_folder/genes.py
+++ files_folder/genes.py
@@ -17,17 +17,30 @@ def compl_strand(dna_seq):
 	
 	#PART1: Create Complementary DNA Sequence
 	compl_dna_seq = ''
-	for nucleotide in compl_dna_seq:
+	for nucleotide in dna_seq:
 		if nucleotide == 'A':
-			compl_dna_seq = sis_dna + "T"
+			compl_dna_seq = compl_dna_seq + "T"
 		elif nucleotide == 'T':
-			compl_dna_seq = sis_dna + "A"
+			compl_dna_seq = compl_dna_seq + "A"
 		elif nucleotide == 'C':
-			compl_dna_seq = sis_dna + "G"
+			compl_dna_seq = compl_dna_seq + "G"
 		elif nucleotide == 'G':
-			compl_dna_seq = sis_dna + "C"
+			compl_dna_seq = compl_dna_seq + "C"
 	
 	return compl_dna_seq
+
+def dna2rna(dna_seq):
+	mRNA_seq = ''
+	for nucleotide in dna_seq:
+		if nucleotide == 'A':
+			mRNA_seq = mRNA_seq + "T"
+		elif nucleotide == 'T':
+			mRNA_seq = mRNA_seq + "A"
+		elif nucleotide == 'C':
+			mRNA_seq = mRNA_seq + "G"
+		elif nucleotide == 'G':
+			mRNA_seq = mRNA_seq + "C"
+	return mRNA_seq
 
 print(compl_strand(dna))
 
