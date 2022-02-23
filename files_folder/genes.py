f337251c799f993d8da18439c7f8a77cf5300923
kh-19 <64449750+kh-19@users.noreply.github.com>
2022-02-23 11:35:44 -0800

    Update genes.py

--- files_folder/genes.py
+++ files_folder/genes.py
@@ -46,5 +46,83 @@ def dna2rna(dna_seq):
 print(compl_strand(dna))
 print(dna2rna(dna))
 
+rna_seq = 'AUG CUG'
+aa_seq = ''
+
+rna_seq = rna_seq.split()
+
+aa_list = {
+	'UUU': 'F',
+	'UUC': 'F',
+	'UUA': 'L',
+	'UUG': 'L',
+	'UCU': 'S',
+	'UCC': 'S',
+	'UCA': 'S',
+	'UCG': 'S',
+	'UAU': 'Y',
+	'UAC': 'Y',
+	'UAA': '',
+	'UAG': '',
+	'UGU': 'C',
+	'UGC': 'C',
+	'UGA': '',
+	'UGG': 'W',
+	'CUU': 'L',
+	'CUC': 'L',
+	'CUA': 'L',
+	'CUG': 'L',
+	'CCU': 'P',
+	'CCC': 'P',
+	'CCA': 'P',
+	'CCG': 'P',
+	'CAU': 'H',
+	'CAC': 'H',
+	'CAA': 'Q',
+	'CAG': 'Q',
+	'CGU': 'R',
+	'CGC': 'R',
+	'CGA': 'R',
+	'CGG': 'R',
+	'AUU': 'I',
+	'AUC': 'I',
+	'AUA': 'I',
+	'AUG': 'M',	
+	'ACU': 'T',
+	'ACC': 'T',
+	'ACA': 'T',
+	'ACG': 'T',
+	'AAU': 'N',
+	'AAC': 'N',
+	'AAA': 'K',
+	'AAG': 'K',
+	'AGU': 'S',
+	'AGC': 'S',
+	'AGA': 'R',
+	'AGG': 'R',
+	'GUU': 'V',
+	'GUC': 'V',
+	'GUA': 'V',
+	'GUG': 'V',
+	'GCU': 'A',
+	'GCC': 'A',
+	'GCA': 'A',
+	'GCG': 'A',
+	'GAU': 'D',
+	'GAC': 'D',
+	'GAA': 'E',
+	'GAG': 'E',
+	'GGU': 'G',
+	'GGC': 'G',
+	'GGA': 'G',
+	'GGG': 'G'
+}
+
+for i in rna_seq:
+	aa = aa_list[i]
+	aa_seq = aa_seq + aa
+
+print(aa_seq)
+
 #close file
 dna_file.close()
