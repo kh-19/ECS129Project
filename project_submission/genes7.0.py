7412f5efd0abfb9eb104a6edac0d4f56ed7dd09a
kh-19 <64449750+kh-19@users.noreply.github.com>
2022-03-02 15:58:14 -0800

    Update genes7.0.py

--- project_submission/genes7.0.py
+++ project_submission/genes7.0.py
@@ -1,62 +1,84 @@
+#import re - a built in Python regular expressions module that helps to search through strings
 import re
-#importing modify_gene function to allow users to create mutations given longest ORF 
+#importing all functions from the genemodifyV5 python program 
 from genemodifyV5 import *
 
-def main():
-        #import file
-        while True:
-	        fname = input("Name of DNA File: ")
-	        try:
-	        	dna_file = open(fname, "r")
-	        	break
-	        except:
-	        	print("Incorrect file name. Make sure you are entering the name of a .txt file (e.g. \'dna.txt\'): ")
-	        	continue
-        print('in file: ',dna_file);
 
+def main():
+        #import DNA file
+        while True:
+            #prod user to input DNA file
+	        fname = input("Name of DNA File: ")
+	        
+	        #check if the DNA file name is legit
+	        try:
+	            #open the DNA file
+	        	dna_file = open(fname, "r")
+	        	#if successful, exit out of the while loop!
+	        	break
+	        	
+	        except:
+	            #if unsuccessful (some error raised), tell user that their filename didn't work
+	        	print("Incorrect file name. Make sure you are entering the name of a .txt file (e.g. \'dna.txt\'): ")
+	        	#start over from beginning of the while loop and ask user again what their filename was
+	        	continue
+	    
+	    #Show user the input file to make sure they picked the right one
+        print('in file: ',dna_file);
+        
+        #initialize a blank DNA strand (we will add nucleotides to this strand)
         dna_strand = ""
 
-        #localize file contents to variables
+        #search through the lines in the DNA file (should only be one line)
         for i in dna_file:
-                
-                #get a DNA strand
+                #turn the sequence of nucleotides uppercase (eliminate lowercase letters)
                 dna_strand = i
                 dna_strand = dna_strand.upper()
                 
-                #check DNA strand for miscellaneous characters
+                #check DNA strand for special characters or numbers - for example '1' or '$'
                 if dna_strand.isalpha() == False:
+                	#if special characters or numbers detected in DNA sequence, alert user and kill the program
                 	print('Faulty input. Special Characters or Numbers detected in DNA input. Check your DNA sequence and start again.')
                 	break
                 
-                #define what characters are not allowed (only A, T, G, C allowed)
-                bad_chars_list = ['B','D','E','F','H','I','J','K','L','M','O','P','Q','R','S','U','V','W','X','Y','Z','/s']
+                #check DNA strand for wrong letters - i.e. anything other than A, C, G, and T
+                #create a list of unacceptable characters or spaces
+                bad_chars_list = ['B','D','E','F','H','I','J','K','L','M','O','P','Q','R','S','U','V','W','X','Y','Z',' ']
+                
+                #use the power of the re module to search if any of the bad characters are present in the DNA sequence
                 for i in bad_chars_list:
                     bad_chars = re.compile(i)
+                    #if none of the bad characters were found in the DNA sequence - the sequence is clean! yay!
                     if(bad_chars.search(dna_strand) == None):
+                        #we can move on to the next part
                         continue
+                    #if a bad character is found, alert the user
                     else:
-                        print("Warning: erroneous input detected! Please check DNA sequence and try again.")
+                        print("Warning: erroneous nucleotides detected! Please check DNA sequence and try again.")
                         
-                        if i == '\s':
+                        #send a special message to the user if a space was the problem
+                        if i == ' ':
                             print("Unauthorized Space detected in Sequence")
                             return
-                            
+                        #tell the user which problematic character raised the error
                         print("Problematic Character Detected:", i)
                         return
+                
+                #if the DNA strand is clean - tell the user so they can celebrate!
                 print("No erroneous DNA input!")
                 
-                #print DNA strand recieved
+                #print input DNA strand so that the user can see it
                 print("Cleaned DNA Strand:", dna_strand)
 
-                #get complementary DNA strand (normal and reversed) 
+                #get and print complementary DNA strand so that the user can see it
                 compl_dna_strand = compl_strand(dna_strand)
                 print("Cleaned Complementary DNA Strand: ", compl_dna_strand)
                 
+                #get and print reversed complementary DNA strand so that the user can see it
                 reversed_compl_dna_strand = compl_dna_strand[::-1]
                 print("Reversed Complementary DNA Strand: ", reversed_compl_dna_strand)
 
-                #you need to process dna_strand and reversed_compl_dna_strand!
-
+                #search for the ORF in both the DNA and reversed Complementary DNA strands
                 gene = process_all_seq(dna_strand, reversed_compl_dna_strand)
                 
                 #neither sequence has start + stop codon present
@@ -89,6 +111,7 @@ def compl_strand(dna_seq):
 
         #PART1: Create Complementary DNA Sequence
         compl_dna_seq = ''
+        #For each Nucleotide in the Sequence, identify what the nucleotide is, and append the corresponding nucleotide to the complementary strand
         for nucleotide in dna_seq:
                 if nucleotide == 'A':
                         compl_dna_seq = compl_dna_seq + "T"
@@ -100,7 +123,8 @@ def compl_strand(dna_seq):
                         compl_dna_seq = compl_dna_seq + "C"
         return compl_dna_seq
 
-
+#generating ORF's from complementary strand (through calling below functions); afterwards, comparing ORF's found 
+# on complementary strand with original strand to find longest ORF 
 def process_all_seq(dna_seq, compl_dna_seq):
 
         #trying to find valid open reading frame from DNA sequence 
@@ -111,9 +135,8 @@ def process_all_seq(dna_seq, compl_dna_seq):
 
 
         #trying to find valid open reading frame from complimentary DNA sequence
-
-        # PK: even if you found a gene on the input strand, the complementary strand could still contain
-        #     a gene that is longer!
+        #even if you found a gene on the input strand, the complementary strand could still contain
+        #a gene that is longer!
 
         gene1 = process_seq(compl_dna_seq)
         if len(gene1) > len(gene):
@@ -121,7 +144,8 @@ def process_all_seq(dna_seq, compl_dna_seq):
 
         return gene
 
-
+#calling find_ORF to generate 3 ORFs for original DNA strand (by shifting the index for each call); using longest gene found from each ORF 
+#and comparing their length to one another 
 def process_seq(dna_seq):
         num_of_triplets = int(len(dna_seq) / 3)
 
@@ -137,6 +161,7 @@ def process_seq(dna_seq):
           gene = gene1
         return gene
 
+#generates open reading frame given dna sequence 
 def find_ORF(dna_seq):
 
         s = ""
@@ -191,7 +216,7 @@ def dna2rna(dna_seq):
 
 #translate mRNA into amino acid sequence
 def RNAtoAA(rna_seq):
-
+        #break mRNA into triplets so we can get codons
         triplet = ''
         rna_seq_list = []
         for i in range(0, int(len(rna_seq) / 3)):
@@ -199,7 +224,8 @@ def RNAtoAA(rna_seq):
                 triplet = rna_seq[j:j+3]
                 rna_seq_list.append(triplet)
                 triplet = ""
-
+        
+        #here is a dictionary with codons as 'keys' and the one letter amino acid code as the 'value'
         aa_list = {
                 'UUU': 'F',
                 'UUC': 'F',
@@ -268,7 +294,8 @@ def RNAtoAA(rna_seq):
         }
 
         aa_seq = ""
-
+        
+        #find corresponding values to each codon 'key' and add it to the aa_seq
         for triplet in rna_seq_list:
                 aa = aa_list[triplet]
                 aa_seq = aa_seq + aa
